1c685636af52602453dae354dd83ad3272345acc
alim509 <tul56193@temple.edu>
2024-03-28 11:42:37 -0400

    Updated package files and added Axios to App.js

--- speakeasyapp/src/App.js
+++ speakeasyapp/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
